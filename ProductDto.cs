00c4d285184bba0f79d8cf7455ae7fbde835cde6
Srikha55 <srikha.ragoonundun@gmail.com>
2024-11-11 11:05:57 +0400

    Add files via upload

---
+++ ProductDto.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebAPI.Models
+{
+    public class ProductDto
+    {
+       
+        [Required]
+        public String Name { get; set; } = " ";
+        [Required]
+        public String Brand { get; set; } = " ";
+        [Required]
+        public String Category { get; set; } = " ";
+        [Required]
+        public decimal Price { get; set; } 
+        [Required]
+        public String Description { get; set; } = " ";
+
+
+    }
+}
